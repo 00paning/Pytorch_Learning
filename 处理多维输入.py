b01a216281326738a30e6f473a5604b319bb04d3
00paning <163004918+00paning@users.noreply.github.com>
2025-03-18 14:01:00 +0800

    Add files via upload

---
+++ 处理多维输入.py
@@ -0,0 +1,35 @@
+import numpy as np
+import torch
+import pandas as pd
+
+xy = pd.read_csv(r"C:\Users\pszpszpsz\Desktop\diabetes.csv").values
+x_data = torch.from_numpy(xy[:,:-1]).float()
+y_data = torch.from_numpy(xy[:,[-1]]).float()
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model,self).__init__()
+        self.linear1 = torch.nn.Linear(8,6)
+        self.linear2 = torch.nn.Linear(6, 4)
+        self.linear3 = torch.nn.Linear(4, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self,x):
+        x = self.sigmoid(self.linear1(x))
+        x = self.sigmoid(self.linear2(x))
+        x = self.sigmoid(self.linear3(x))
+        return x
+
+model = Model()
+
+criterion = torch.nn.BCELoss(size_average=True)
+optimizer = torch.optim.SGD(model.parameters(),lr=1)
+
+for epoch in range(100):
+    y_pred = model(x_data)
+    loss = criterion(y_pred,y_data)
+    print(epoch,loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
